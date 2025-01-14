6cd4215a6a403d43ba545f134c1a0b2d134254c3
Shiva Sharma <ss12@umbc.edu>
2025-01-14 16:26:58 -0500

    updated code

--- src/main/java/com/example/apimanagement/config/WebConfig.java
+++ src/main/java/com/example/apimanagement/config/WebConfig.java
@@ -6,17 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-   @Value("${frontend.url}")
-   private String  frontendUrl;
-
-    @Value("${backend.url}")
-    private String  backendUrl;
-
-
-    @Override
+      @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins(frontendUrl,backendUrl) // Allow requests from your React app
+                .allowedOrigins("http://0.0.0.0:3000","http://0.0.0.0:8080") // Allow requests from your React app
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow cookies and credentials
